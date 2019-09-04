fe8ced3cd979b35e7213cd39df3f22de864b1156
Aniket Aryamane <aryamane.aniket@gmail.com>
2019-09-04 18:50:56 +0530

    Django Tutorials 23. Form - Signup (urdu/hindi)

--- allapps/studentbook/urls.py
+++ allapps/studentbook/urls.py
@@ -7,4 +7,5 @@ app_name = 'studentbook'
 urlpatterns = [
     path('', views.student_login, name='student_login'),
     path('signup/', views.student_signup, name='student_signup'),
+    path('register/', views.register_student, name='register_student'),
 ]
--- allapps/studentbook/views.py
+++ allapps/studentbook/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -8,3 +9,17 @@ def student_login(request):
 
 def student_signup(request):
     return render(request, 'studentbook/signup.html', {})
+
+
+def register_student(request):
+    if request.POST:
+        username = request.POST['student_name']
+        email = request.POST['student_email']
+        password = request.POST['student_password']
+
+        obj = User(username=username, email=email, password=password)
+        obj.set_password(password)
+        obj.save()
+
+        return render(request, 'studentbook/register_student.html', {})
+    return redirect('studentbook:student_login')
